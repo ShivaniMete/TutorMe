b4264f570697723618c40c07ae7643513b58eb4c
ndatoxicity <magicfreak21@gmail.com>
2014-11-03 10:18:05 -0600

    cleaned out courseController class and added addCourse.jsp page

---
+++ src/main/java/com/myprojects/tutorme/dao/CourseRowMapper.java
@@ -0,0 +1,21 @@
+package com.myprojects.tutorme.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.myprojects.tutorme.model.Course;
+
+public class CourseRowMapper implements RowMapper<Course>{
+
+	//@Override -- To use this change Java Compiler compliance to 1.6 
+	public Course mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub			
+		Course dbCourse = new Course();
+		dbCourse.setCourseId(resultSet.getString(1));
+		dbCourse.setCourseName(resultSet.getString(2));
+		dbCourse.setCourseCategory(resultSet.getString(3));
+		return dbCourse;	
+	}
+}
